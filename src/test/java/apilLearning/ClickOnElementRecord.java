5a850c0137b383086240072779db2a8c894e5721
Minh Le <v-lminh@microsoft.com>
2023-05-01 15:54:14 +0700

    recording videos

--- src/test/java/apilLearning/ClickOnElementRecord.java
+++ src/test/java/apilLearning/ClickOnElementRecord.java
@@ -3,6 +3,7 @@ package apilLearning;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.screenrecording.CanRecordScreen;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
@@ -10,7 +11,6 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.interactions.Action;
 import utils.BasePage;
 
 import java.io.File;
@@ -21,49 +21,49 @@ import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Base64;
 
-public class ClickOnElement {
+public class ClickOnElementRecord {
     public static void main(String[] args) throws InterruptedException, IOException {
-        AppiumDriver<MobileElement> appiumDriver = BasePage.getAppiumDriver();
-        ((CanRecordScreen) appiumDriver).startRecordingScreen();
+        AndroidDriver<MobileElement> androidDriver = BasePage.getAndroidDriver();
+        ((CanRecordScreen) androidDriver).startRecordingScreen();
         Thread.sleep(10000);
-        MobileElement loginLabel = appiumDriver.findElementByAccessibilityId("Login");
+        MobileElement loginLabel = androidDriver.findElementByAccessibilityId("Login");
         loginLabel.click();
 
         Thread.sleep(3000);
         //Enter email
-        MobileElement email = appiumDriver.findElementByAccessibilityId("input-email");
+        MobileElement email = androidDriver.findElementByAccessibilityId("input-email");
         System.out.println("Email Test is " + email.getText());
         email.sendKeys("ledacminh0305@gmail.com");
 
         Thread.sleep(6000);
         //Enter passsword
-        MobileElement password = appiumDriver.findElementByAccessibilityId("input-password");
+        MobileElement password = androidDriver.findElementByAccessibilityId("input-password");
         password.sendKeys("password");
 
         Thread.sleep(2000);
         //click button Login
-        MobileElement buttonLogin = appiumDriver.findElementByAccessibilityId("button-LOGIN");
+        MobileElement buttonLogin = androidDriver.findElementByAccessibilityId("button-LOGIN");
         buttonLogin.click();
 
         Thread.sleep(2000);
-        MobileElement successMessage = appiumDriver.findElementById("android:id/alertTitle");
+        MobileElement successMessage = androidDriver.findElementById("android:id/alertTitle");
         System.out.println("Message login successfully " + successMessage.getText());
 
-        File formScreenBase64Data = ((TakesScreenshot) appiumDriver).getScreenshotAs(OutputType.FILE);
+        File formScreenBase64Data = ((TakesScreenshot) androidDriver).getScreenshotAs(OutputType.FILE);
         String formScreenPath = System.getProperty("user.dir") + "/screenshot/" + "formScreen.png";
         System.out.println("Path: " + formScreenPath);
         FileUtils.copyFile(formScreenBase64Data, new File(formScreenPath));
 
-        MobileElement buttonOK = appiumDriver.findElementById("android:id/button1");
+        MobileElement buttonOK = androidDriver.findElementById("android:id/button1");
         buttonOK.click();
 
-        MobileElement buttonForm = appiumDriver.findElementByXPath("//*[@text='Forms']");
+        MobileElement buttonForm = androidDriver.findElementByXPath("//*[@text='Forms']");
         buttonForm.click();
 
         Thread.sleep(3000);
 
         //Scroll up and down
-        Dimension windownSize = appiumDriver.manage().window().getSize();
+        Dimension windownSize = androidDriver.manage().window().getSize();
         int screenHeight = windownSize.getHeight();
         int screenWidth = windownSize.getWidth();
         int xStartPoint = 50 * screenWidth / 100;
@@ -78,7 +78,7 @@ public class ClickOnElement {
 
         PointOption startPoint = new PointOption().withCoordinates(xStartPoint, yStartPoint);
         PointOption endPoint = new PointOption().withCoordinates(xEndPoint, yEndPoint);
-        TouchAction touchAction = new TouchAction(appiumDriver);
+        TouchAction touchAction = new TouchAction(androidDriver);
         touchAction
                 .press(startPoint)
                 .waitAction(new WaitOptions().withDuration(Duration.ofSeconds(1)))
@@ -95,7 +95,7 @@ public class ClickOnElement {
                 .perform();
 
 
-        String video = ((CanRecordScreen)appiumDriver).stopRecordingScreen();
+        String video = ((CanRecordScreen)androidDriver).stopRecordingScreen();
         //Save the records video\
         byte[] decodevideo = Base64.getMimeDecoder().decode(video);
         String videoDir = System.getProperty("user.dir") + "/videos";
--- src/test/java/utils/BasePage.java
+++ src/test/java/utils/BasePage.java
@@ -2,11 +2,13 @@ package utils;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class BasePage {
 
@@ -25,9 +27,32 @@ public class BasePage {
             //Set up the appium server url to connect
             URL appiumServer = new URL("http://localhost:4723/wd/hub");
             appiumDriver = new AppiumDriver<>(appiumServer, desiredCapabilities);
+            appiumDriver.manage().timeouts().implicitlyWait(30L, TimeUnit.SECONDS);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
         return appiumDriver;
+    }
+
+    public static AndroidDriver<MobileElement> getAndroidDriver() {
+        AndroidDriver<MobileElement> androidDriver;
+        try {
+            //Setup setCapability
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.PLATFORM_NAME, "Android");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.UDID, "emulator-5554");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.AUTOMATION_NAME, "uiautomator2");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.APP_PACKAGE, "com.wdiodemoapp");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.APP_ACTIVITY, "com.wdiodemoapp.MainActivity");
+            desiredCapabilities.setCapability(MobileCapabilityTypeEx.NEW_COMMAND_TIMEOUT, 60);
+
+            //Set up the appium server url to connect
+            URL androidServer = new URL("http://localhost:4723/wd/hub");
+            androidDriver = new AndroidDriver<>(androidServer, desiredCapabilities);
+            androidDriver.manage().timeouts().implicitlyWait(30L, TimeUnit.SECONDS);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        return androidDriver;
     }
 }
